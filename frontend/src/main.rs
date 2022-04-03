1e336a856f00a79b6127e594f72ada64377ec2db
Robert Krahn <robert.krahn@gmail.com>
2022-04-03 22:43:25 +0200

    frontend setup

---
+++ frontend/src/main.rs
@@ -0,0 +1,30 @@
+use yew::prelude::*;
+use yew_router::prelude::*;
+
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/")]
+    Home,
+}
+
+fn switch(routes: &Route) -> Html {
+    match routes {
+        Route::Home => html! { <h1>{ "Hello Frontend" }</h1> },
+    }
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <BrowserRouter>
+            <Switch<Route> render={Switch::render(switch)} />
+        </BrowserRouter>
+    }
+}
+
+fn main() {
+    wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
+    console_error_panic_hook::set_once();
+
+    yew::start_app::<App>();
+}
