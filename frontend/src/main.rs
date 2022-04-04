0f24f9778afb8622d037334929fa446f5ec302de
Robert Krahn <robert.krahn@gmail.com>
2022-04-04 02:48:44 +0200

    frontend queries /api/hello

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,5 @@
+use reqwasm::http::Request;
+use wasm_bindgen_futures::spawn_local;
 use yew::prelude::*;
 use yew_router::prelude::*;
 
@@ -5,11 +7,14 @@ use yew_router::prelude::*;
 enum Route {
     #[at("/")]
     Home,
+    #[at("/hello-server")]
+    HelloServer,
 }
 
 fn switch(routes: &Route) -> Html {
     match routes {
         Route::Home => html! { <h1>{ "Hello Frontend" }</h1> },
+        Route::HelloServer => html! { <HelloServer /> },
     }
 }
 
@@ -19,6 +24,55 @@ fn app() -> Html {
         <BrowserRouter>
             <Switch<Route> render={Switch::render(switch)} />
         </BrowserRouter>
+    }
+}
+
+#[function_component(HelloServer)]
+fn hello_server() -> Html {
+    let data = use_state(|| None);
+
+    // Request `/api/hello` once
+    {
+        let data = data.clone();
+        use_effect(move || {
+            if data.is_none() {
+                spawn_local(async move {
+                    let resp = Request::get("/api/hello").send().await.unwrap();
+                    let result = {
+                        if !resp.ok() {
+                            Err(format!(
+                                "Error fetching data {} ({})",
+                                resp.status(),
+                                resp.status_text()
+                            ))
+                        } else {
+                            resp.text().await.map_err(|err| err.to_string())
+                        }
+                    };
+                    data.set(Some(result));
+                });
+            }
+
+            || {}
+        });
+    }
+
+    match data.as_ref() {
+        None => {
+            html! {
+                <div>{"No server response"}</div>
+            }
+        }
+        Some(Ok(data)) => {
+            html! {
+                <div>{"Got server response: "}{data}</div>
+            }
+        }
+        Some(Err(err)) => {
+            html! {
+                <div>{"Error requesting data from server: "}{err}</div>
+            }
+        }
     }
 }
 
