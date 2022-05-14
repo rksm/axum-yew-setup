b283d1a334a82c645bc7af2fbe7dc13f5fdf7252
Robert Krahn <robert.krahn@gmail.com>
2022-05-14 03:54:27 +0200

    workspace works when setting CARGO_TARGET_DIR

--- dev.sh
+++ dev.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 IFS=$'\n\t'
 
 (trap 'kill 0' SIGINT; \
- bash -c 'cd frontend; trunk serve' & \
+ bash -c 'cd frontend; CARGO_TARGET_DIR=../target-trunk trunk serve --address 0.0.0.0' & \
  bash -c 'cd server; cargo watch -- cargo run -- --port 8081')
