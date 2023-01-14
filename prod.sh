ea86d9eb34c87164058201d81d7112f43e25e965
Robert Krahn <robert.krahn@gmail.com>
2023-01-14 21:07:05 +0100

    fix prod.sh script, fixes #6

--- prod.sh
+++ prod.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 pushd frontend
-CARGO_TARGET_DIR=../target-trunk trunk build --release --public-url /assets/
+CARGO_TARGET_DIR=../target-trunk trunk build --release --public-url /
 popd
 
-cargo run --bin server --release -- --port 8080
+cargo run --bin server --release -- --port 8080 --static-dir ./dist
